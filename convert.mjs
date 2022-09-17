1315658ebaf960f1d57b7f06e8aae236965d9206
Sean Martin <29813268+wishcresp@users.noreply.github.com>
2022-09-17 23:29:16 +1000

    Update config

--- convert.mjs
+++ convert.mjs
@@ -1,6 +1,7 @@
-const fs = require("fs");
-const util = require("util");
-const exec = util.promisify(require("child_process").exec);
+import {exec as asyncExec} from "child_process";
+import fs from "fs";
+import util from "util";
+const exec = util.promisify(asyncExec);
 
 const CONFIG_DIR = "config";
 const OUTPUT_DIR = "output";
@@ -13,7 +14,7 @@ const entityTypes = new Map([
   ["summitGem", "g"],
 ]);
 
-const areaArg = process.argv[1];
+const areaArg = process.argv[2];
 
 // Load the import config.
 const {id, areaId, name, author, link, chapters} = JSON.parse(fs.readFileSync(`./${CONFIG_DIR}/${areaArg}.json`, "utf-8"));
@@ -36,50 +37,37 @@ for (const {id, gameId, name, sides} of chapters) {
   };
 
   for (const {file, checkpoints} of sides) {
-    const {stdout} = await exec(`julia loadMap.jl ${CONFIG_DIR}/${file}.bin`);
+    const {stdout} = await exec(`julia loadMap.jl maps/${file}.bin`, {maxBuffer: 64 * 1024 * 1024});
     const {rooms} = JSON.parse(stdout);
 
-    const side = {
-      c: checkpoints.map(({name: n, abv: a, order: o}) => ({...n && {n}, ...a && {a}, ...o && {o}})),
-      r: rooms.map(({name, position: [x, y], size: [w, h], entities}) => ({
-        i: name.slice(4),
-        p: {x, y},
-        s: {w, h},
-        e: entities.filter()
+    let cpIdx = 0;
+
+    const c = checkpoints.map(({name: n, abv: a, order: o}) => ({...n && {n}, ...a && {a}, ...o && {o}}));
+    
+    const r = rooms.map(({name, position: [x, y], size: [w, h], entities}) => ({
+      i: name.slice(4),
+      p: {x, y},
+      s: {w, h},
+      e: entities === undefined ? [] : entities.filter(({name}) => {
+        if (name === "checkpoint") {
+          c[cpIdx] = {i: name.slice(4), ...c[cpIdx]};
+          cpIdx++;
+        }
+        return validEntities.has(name);
+      }).map(({name, x, y, checkpointID, order}) => ({
+        t: entityTypes.get(name),
+        x,
+        y,
+        ...checkpointID && {c: checkpointID},
+        ...order && {o: order},
       })),
-    };
+    }));
 
-    for (const {name, position: [x, y], size: [w, h], entities} of rooms) {
-      const debugId = name.slice(4);
-      let cpIdx = 0;
-      
-      const room = {
-        i: debugId,
-        p: {x, y},
-        s: {w, h},
-        e: entities.filter(({name}) => {
-          if (name === "checkpoint") {
-            side.c[cpIdx] = {i: debugId, ...side.c[cpIdx]};
-            cpIdx++;
-          }
-          return validEntities.has(name);
-        }).map(({name, x, y, checkpointID, order}) => ({
-          t: entityTypes.get(name),
-          x,
-          y,
-          ...checkpointID && {c: checkpointID},
-          ...order && {o: order},
-        })),
-      };
-
-      side.r.push(room);
-    };
-
-    chapter.s.push(side);
+    chapter.s.push({c, r});
   };
-
+  
   area.c.push(chapter);
 };
 
 // Write the output.
-fs.writeFileSync(`${OUTPUT_DIR}/${areaKey}.json`, JSON.stringify(area));
+fs.writeFileSync(`${OUTPUT_DIR}/${areaArg}.json`, JSON.stringify(area));
